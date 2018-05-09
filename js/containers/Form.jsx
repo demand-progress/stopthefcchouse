4d718986c9d7bcf012313f2da11a3b8b76500e44
Mateo Balcorta <misterrattail@gmail.com>
2018-05-08 21:47:00 -0700

    Update form submit.

--- js/containers/Form.jsx
+++ js/containers/Form.jsx
@@ -217,7 +217,7 @@ class Form extends Component {
             form.appendChild(input);
         });
 
-        // form.submit()
+        form.submit()
         this.setState({ submitted: true });
     }
 
