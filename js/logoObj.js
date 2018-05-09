19eb320122ad3a1f21d97b143997c36222f5f29f
Mateo Balcorta <misterrattail@gmail.com>
2018-05-09 10:58:44 -0700

    Create logos object with alt text and src text.

---
+++ js/logoObj.js
@@ -0,0 +1,31 @@
+const logoObj = [
+    { alt: "18 million rising", src: "images/18mr_logo_short.png"},
+    { alt: "American Family Voices (AFV)", src: "images/PasAFV.jpg"},
+    { alt: "Center for Media Justice", src: "images/CMJ logo hi-res.jpg"},
+    { alt: "Center for Popular Democracy Action", src: "images/cpd-action-logo.png"},
+    { alt: "Climate Hawks Vote", src: "images/CHV Logo2.jpg"},
+    { alt: "Color of Change", src: "images/COC-Full-Color.png"},
+    { alt: "Common Cause", src: "images/CCLogo_Stacked_NoTagline.jpg"},
+    { alt: "Common Dreams", src: "images/CD_stacked_white.jpg"},
+    { alt: "Consumer Reports", src: "images/consumer-reports.png"},
+    { alt: "Corporate Accountability", src: "images/CA_LogoTag_Large.png"},
+    { alt: "Courage Campaign", src: "images/Courage-Logo-Color-High-Rez.jpg"},
+    { alt: "Democracy for America", src: "images/DFA-Logo-bottom-white-400.png"},
+    { alt: "Friend of the Earth", src: "images/FOE_logo_color.jpg"},
+    { alt: "Just Foreign Policy", src: "images/just-foreign-policy.jpg"},
+    { alt: "Open Media", src: "images/open-media.jpg"},
+    { alt: "Democracy for America", src: "images/DFA-Logo-bottom-white-400.png"},
+    { alt: "People For the American Way", src: "images/PFAW.png"},
+    { alt: "Presente Action", src: "images/PresenteAction2016.png"},
+    { alt: "Progress America", src: "images/progress-america-logo-1.png"},
+    { alt: "Progressive Congress Action Fund", src: "images/IMG_8465.jpg"},
+    { alt: "RootsAction.org", src: "images/RootsAction.png"},
+    { alt: "The Nation", src: "images/Nation.jpg"},
+    { alt: "The Zero Hour", src: "images/TZH Logo.jpg"},
+    { alt: "Watchdog", src: "images/LOGO_Watchdog-v04.png"},
+    { alt: "Win Without War", src: "images/winwithoutwar.jpg"},
+    { alt: "Workign Families Party", src: "images/wfp.jpg"}
+
+]
+
+export default logoObj; 
